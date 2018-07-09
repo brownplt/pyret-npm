e75e8ece45dd61f5c49bda9e5674c6f0054b1a60
Joe Gibbs Politz <joe.politz@gmail.com>
2018-07-09 10:54:13 -0700

    install npm deps and run build

--- build.js
+++ build.js
@@ -2,7 +2,9 @@ var childProcess = require("child_process");
 var fs = require("fs");
 
 if (!fs.existsSync("pyret-lang")) {
-  childProcess.spawnSync("git", ["clone", "--single-branch", "-b", "server-dev", "https://github.com/brownplt/pyret-lang.git"], {'stdio': 'inherit'})
+  childProcess.spawnSync("git", ["clone", "--single-branch", "-b", "server-dev", "https://github.com/brownplt/pyret-lang.git"], {'stdio': 'inherit'});
 }
 
-childProcess.spawnSync("make", ["-C", "pyret-lang", "phaseA", "libA"], {'stdio': 'inherit'})
+childProcess.spawnSync("npm", ["install"], {'stdio': 'inherit', 'cwd': "pyret-lang"});
+childProcess.spawnSync("make", ["-C", "pyret-lang", "phaseA", "libA"], {'stdio': 'inherit'});
+
