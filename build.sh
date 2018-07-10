8a280cab8cdebe9be965fb90a747358b60f25314
Joe Gibbs Politz <joe.politz@gmail.com>
2018-07-10 09:27:41 -0700

    put deps in package.json, remove unnecessary file, clean build script

--- build.sh
+++ build.sh
@@ -3,10 +3,7 @@
 git clone --single-branch -b server-dev https://github.com/brownplt/pyret-lang.git
 
 pushd pyret-lang
-node lib/jglr/rnglr.js
 npm install
 make phaseA libA
 popd
-npm-install-all pyret.js
-npm-install-all client-lib.js
 
